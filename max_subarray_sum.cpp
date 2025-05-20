22e67b0e08f2e98e0ff0ca7b1a899d3d5997d404
SHIVAM TANDON <shivamtandonn@gmail.com>
2025-05-20 13:50:23 +0530

    Maximum Subarray Sum, Brute Force Method

---
+++ max_subarray_sum.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(int arr[],int size){
+    for(int start=0; start<size; start++){
+        for(int end=start; end<size; end++){
+            for(int i=start; i<=end; i++){
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << "  ";
+    }
+    cout << " " << endl;
+}
+
+int max_subarray_sum(int arr[], int size, int MAX){
+    for(int start=0; start<size; start++){
+        int currSum=0;
+        for(int end=start; end<size; end++){
+            currSum+=arr[end];
+            MAX=max(currSum,MAX);
+        }
+    }
+    return MAX;
+}
+
+int main(){
+    int size;
+    cout<< "Enter no. of elements in an array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter elements in an array: ";
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+    cout << "_ _ _" << endl;
+    int MAX=0;
+    int choice;
+    while(choice== 1 or 2){
+        cout << "What you want me to do next??" << "\n" << "1. Display all subarrays" << "\n" << "2. Find out the maximum subarray sum" << "\n" << "3. Exit" << "\n" << "\n" << "Enter your choice as 1,2 or 3: ";
+    cin >> choice;
+    cout << " " << endl;
+    if(choice==1){
+        display(arr,size);
+        cout << "_ _ _" << endl;
+    }
+    else if(choice==2){
+        MAX=max_subarray_sum(arr,size,MAX);
+        cout << "The maximum subarray sum is: " << MAX << endl;
+        cout << "_ _ _" << endl;
+    }
+    else if(choice==3){
+        break;
+    }
+    else{
+        cout << "Invalid Choice" << endl;
+    }
+    }
+    return 0;
+}
