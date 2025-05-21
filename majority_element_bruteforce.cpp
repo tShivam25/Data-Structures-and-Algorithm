01cb8a083667bdc9497ea5becd4709acc00a27bb
SHIVAM TANDON <shivamtandonn@gmail.com>
2025-05-21 09:03:09 +0530

    Majority Element by Brute Force Approach

--- majority_element_bruteforce.cpp
+++ majority_element_bruteforce.cpp
@@ -32,7 +32,6 @@ int main(){
     for(int i=0; i<size; i++){
         cin >> arr[i];
     }
-    int go;
     cout << "Lets find out the majority element in the array!!!!" << endl;
     vector <int> majority=MajorityElement(arr,size);
     if(majority[0]==-1){
